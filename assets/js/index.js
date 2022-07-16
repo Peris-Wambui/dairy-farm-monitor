07bcba1006a54dea1e90aab23e7e1b4655376e70
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-07-16 00:03:03 -0400

    give each shed paragraph id selector

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,19 +3,19 @@ function totalProduction(){
     let shedA = document.getElementById("sheda").value;
     console.log(shedA);
     // the .innerHTML embeds/atouch a text in the paragraph 
-    document.getElementById("reportP").innerHTML += "Your production in  shed A  is " + shedA + " liters per day"
+    document.getElementById("reportA").innerHTML += "Your production in  shed A  is " + shedA + " liters per day."
 
     let shedB = document.getElementById("shedb").value;
     console.log(shedB);
-    document.getElementById("reportP").innerHTML += "Your production in  shed B is " + shedB + " liters per day"
+    document.getElementById("reportB").innerHTML += "Your production in  shed B is " + shedB + " liters per day."
 
 
     let shedC = document.getElementById("shedc").value;
     console.log(shedC);
-    document.getElementById("reportP").innerHTML += "Your production in  shed C is " + shedC + " liters per day"
+    document.getElementById("reportC").innerHTML += "Your production in  shed C is " + shedC + " liters per day."
 
     let shedD = document.getElementById("shedd").value;
     console.log(shedD);
-    document.getElementById("reportP").innerHTML += "Your production in  shed D is " + shedD + " liters per day"
+    document.getElementById("reportD").innerHTML += "Your production in  shed D is " + shedD + " liters per day."
 
 }
