bd9007f9f2fdb60675f498b2a95f22d98cff2fb8
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-07-15 23:32:54 -0400

    get value of all shed using their id

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,14 @@
 function totalProduction(){
     event.preventDefault();
-    let shedA = document.getElementById("sheda").value
+    let shedA = document.getElementById("sheda").value;
     console.log(shedA);
+
+    let shedB = document.getElementById("shedb").value;
+    console.log(shedB);
+
+    let shedC = document.getElementById("shedc").value;
+    console.log(shedC);
+
+    let shedD = document.getElementById("shedd").value;
+    console.log(shedD);
 }
