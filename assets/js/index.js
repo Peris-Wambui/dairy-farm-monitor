d7c09e3dc81b87137360199c64616aeefc8d06fa
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-07-15 23:50:45 -0400

    display  entered result to the browser

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,13 +2,20 @@ function totalProduction(){
     event.preventDefault();
     let shedA = document.getElementById("sheda").value;
     console.log(shedA);
+    // the .innerHTML embeds/atouch a text in the paragraph 
+    document.getElementById("reportP").innerHTML += "Your production in  shed A  is " + shedA + " liters per day"
 
     let shedB = document.getElementById("shedb").value;
     console.log(shedB);
+    document.getElementById("reportP").innerHTML += "Your production in  shed B is " + shedB + " liters per day"
+
 
     let shedC = document.getElementById("shedc").value;
     console.log(shedC);
+    document.getElementById("reportP").innerHTML += "Your production in  shed C is " + shedC + " liters per day"
 
     let shedD = document.getElementById("shedd").value;
     console.log(shedD);
+    document.getElementById("reportP").innerHTML += "Your production in  shed D is " + shedD + " liters per day"
+
 }
