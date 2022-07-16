1158754643dd424a056d647596c10ae908e4b447
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-07-16 12:36:55 -0400

    use parseInt to convert a string to number

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,23 @@
 function totalProduction(){
     event.preventDefault();
-    let shedA = document.getElementById("sheda").value;
+    let shedA = parseInt (document.getElementById("sheda").value);
     console.log(shedA);
     // the .innerHTML embeds/atouch a text in the paragraph 
     document.getElementById("reportA").innerHTML += "Your production in  shed A  is " + shedA + " liters per day."
 
-    let shedB = document.getElementById("shedb").value;
+    let shedB = parseInt (document.getElementById("shedb").value);
     console.log(shedB);
     document.getElementById("reportB").innerHTML += "Your production in  shed B is " + shedB + " liters per day."
 
 
-    let shedC = document.getElementById("shedc").value;
+    let shedC = parseInt (document.getElementById("shedc").value);
     console.log(shedC);
     document.getElementById("reportC").innerHTML += "Your production in  shed C is " + shedC + " liters per day."
 
-    let shedD = document.getElementById("shedd").value;
+    let shedD = parseInt (document.getElementById("shedd").value);
     console.log(shedD);
     document.getElementById("reportD").innerHTML += "Your production in  shed D is " + shedD + " liters per day."
 
+    let totalDailyLiters = shedA + shedB + shedC + shedD;
+    console.log(totalDailyLiters);
 }
